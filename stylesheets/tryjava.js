40d01b056d7ba2ed2d4ffccd457026c2d2025d6e
ftahery <ftahery93@gmail.com>
2014-12-19 23:24:48 +0530

    css n js

---
+++ stylesheets/tryjava.js
@@ -0,0 +1,11 @@
+
+var chart = new Highcharts.Chart({
+      chart: {
+         renderTo: 'container'
+      },
+      series: [{
+         data: [1,
+         pointStart: 0,
+         pointInterval
+      }]
+});
