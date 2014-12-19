6458c2acca39993bdf509237fb37e3981525439e
ftahery <ftahery93@gmail.com>
2014-12-19 18:21:38 +0530

    first commit

---
+++ aboutus/about_us.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once("../includes/functions.php");
+?>
+
+<html lang="en">
+   <?php
+  $par="About Us";
+  echo head_body_create($par); ?>
+
+  <body>
+
+    <?php
+	$par="About Us";
+	echo navigation_bar($par);
+	?>
+
+
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>About Us</h1>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+     
+        <div class="col-md-4">
+
+          <font size="6">Fakhruddin Tahery</font><br>
+          <font size="4">Email: ftahery93@gmail.com</font><br>
+          <font size="4">USN : 1MS12CS029</font>
+             <br>  <br>
+          <font size="6">Jivitesh Mopuri</font><br>
+          <font size="4">Email: mocolocoroco@gmail.com</font> <br>
+          <font size="4">USN : 1MS12CS042</font>
+                <br> <br>
+          <font size="6">Munir Ali Munir Al Qutob</font><br>
+          <font size="4">Email: muniralqutob@gmail.com</font><br>
+          <font size="4">USN : 1MS12CS061</font>
+
+
+        </div>
+      </div>
+
+
+
+      <footer>
+
+      </footer>
+    </div>
+
+
+   <script src="stylesheets/footer_1.js"></script>
+    <script src="stylesheets/footer_2.js"></script>
+    <script src="stylesheets/footer_3.js"></script>
+  </body>
+</html>
